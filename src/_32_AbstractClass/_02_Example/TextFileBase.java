714a2537983bf34cae020fd93fd31c22959ea171
Cihat Köse <cihat.kose@hotmail.com>
2025-03-15 13:46:34 +0100

    refactor: Improved class structure and naming in abstract class example. - Improved code formatting, indentation, and comments. - Separated 'main' method into a dedicated class.

--- src/_32_AbstractClass/_02_Example/TextFileBase.java
+++
@@ -1,7 +0,0 @@
-package _32_AbstractClass._02_Example;
-
-public abstract class TextFileBase {
-    // Soyut metotlar, alt sınıflar tarafından implement edilmek zorundadır.
-    public abstract void read();
-    public abstract void write();
-}
