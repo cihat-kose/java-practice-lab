69514a2b085eaedea2888ec332521b7e8d5fe63d
Cihat Köse <cihat.kose@hotmail.com>
2025-08-23 19:44:38 +0200

    Rename variable to Turkish and add label before printing third column

--- src/_17_Arrays2D/_03_PrintThirdColumn.java
+++ src/_17_Arrays2D/_03_PrintThirdColumn.java
@@ -9,17 +9,17 @@ package _17_Arrays2D;
  * 6
  * 9
  */
-
 public class _03_PrintThirdColumn {
     public static void main(String[] args) {
-        int[][] matrix = {
+        int[][] matris = {
                 {1, 2, 3},  // 1. satır
                 {4, 5, 6},  // 2. satır
                 {7, 8, 9}   // 3. satır
         };
 
-        for (int i = 0; i < matrix.length; i++) {
-            System.out.println(matrix[i][2]); // 3. sütun elemanlarını yazdır
+        System.out.println("Matrisin 3. sütunundaki elemanlar:");
+        for (int i = 0; i < matris.length; i++) {
+            System.out.println(matris[i][2]); // 3. sütun elemanlarını yazdır
         }
     }
 }
